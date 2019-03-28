29c18c28d3ed017e4b1cbb545d8f64876ee91e44
JustDoIt2019 <46106978+JustDoIt2019@users.noreply.github.com>
2019-03-28 17:27:10 +0800

    version1

---
+++ src/test/java/SetDemo328.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import java.util.*;
+
+public class SetDemo328 {
+    @Test
+    public void test1() {
+        Set set = new HashSet();
+        set.add("abc");
+        set.add("cde");
+        set.add("efg");
+        set.add("fgh");
+        set.add("abc"); //重复的abc,set会自动将其去掉
+        System.out.println("size=" + set.size());
+
+        List list = new ArrayList();
+        list.add("abc");
+        list.add("aaa");
+        list.add("fff");
+        set.addAll(list); //将list中的值加入set,并去掉重复的abc
+        System.out.println("size=" + set.size());
+
+        //迭代器是一种设计模式，它是一个对象，它可以遍历并选择序列中的对象
+        //Java中的Iterator功能比较简单，并且只能单向移动
+        //List Set都可以使用
+        for (Iterator it = set.iterator(); it.hasNext(); ) {
+            System.out.println("value=" + it.next().toString());
+        }
+    }
+}
---
+++ src/test/java/StringDemo328.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.Test;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringDemo328 {
+    String[] s = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+    public int numUniqueEmails(String[] emails) {
+        Set<String> seen = new HashSet();
+        for (String email : emails) {
+            int i = email.indexOf('@');
+            String local = email.substring(0, i);
+            String rest = email.substring(i);
+            if (local.contains("+")) {
+                //local = local.substring(0, local.indexOf('+'));
+                String[] a = local.split("\\+");
+                local = a[0];
+            }
+            local = local.replaceAll("\\pP", "");
+            System.out.println(local);
+            seen.add(local + rest);
+        }
+
+        return seen.size();
+    }
+    @Test
+    public void test(){
+        //String a = "asdf.alsjdf.ueo";
+        //a = a.replaceAll("\\pP","");
+        // a = a.replace(".","");
+        // System.out.println(a);
+        int re = numUniqueEmails(s);
+        System.out.println(re);
+    }
+}
+
+
+
